e998c56b3628e23dbdb054546092d00e7f39015b
Troy Stribling <troy.stribling@gmail.com>
2014-03-04 22:30:23 -0800

    moved uuids to defines

---
+++ HueSwitch/HueSwitchProfile.h
@@ -0,0 +1,13 @@
+//
+//  HueSwitchProfile.h
+//  HueSwitch
+//
+//  Created by Troy Stribling on 3/4/14.
+//  Copyright (c) 2014 gnos.us. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HueSwitchProfile : NSObject
+
+@end
---
+++ HueSwitch/HueSwitchProfile.m
@@ -0,0 +1,13 @@
+//
+//  HueSwitchProfile.m
+//  HueSwitch
+//
+//  Created by Troy Stribling on 3/4/14.
+//  Copyright (c) 2014 gnos.us. All rights reserved.
+//
+
+#import "HueSwitchProfile.h"
+
+@implementation HueSwitchProfile
+
+@end
