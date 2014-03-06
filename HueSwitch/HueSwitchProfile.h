5b1bf6898ad95ea4d9f3cb01cb82b82a5757f9f1
Troy Stribling <troy.stribling@gmail.com>
2014-03-05 22:33:27 -0800

    more gatt work

--- HueSwitch/HueSwitchProfile.h
+++ HueSwitch/HueSwitchProfile.h
@@ -8,8 +8,10 @@
 
 #import <Foundation/Foundation.h>
 
-#define HUE_LIGHTS_SWITCH_SERVICE_UUID  @"2f0a-0010-69aa-f316-3e78-4194989a6c1a"
-#define HUE_LIGHTS_SCENES_SERVICE_UUID  @"2f0a-0021-69aa-f316-3e78-4194989a6c1a"
+#define HUE_LIGHTS_SERVICE_UUID     @"2f0a-0021-69aa-f316-3e78-4194989a6c1a"
+
+#define HUE_LIGHTS_NUMBER_OF_SCENES     @"Number of Scenes"
+#define HUE_LIGHTS_SCENE_ID             @"Scene ID"
 
 @interface HueSwitchProfile : NSObject
 
--- HueSwitch/HueSwitchProfile.m
+++ HueSwitch/HueSwitchProfile.m
@@ -17,63 +17,153 @@
     
 #pragma mark - Switch Service -
     
-    [profileManager createServiceWithUUID:HUE_LIGHTS_SWITCH_SERVICE_UUID
-                                     name:@"Hue Lights Switches"
-                               andProfile:^(BlueCapServiceProfile* serviceProfile) {
-                                   [serviceProfile createCharacteristicWithUUID:@"2f0a0011-69aa-f316-3e78-4194989a6c1a"
-                                                                           name:@"Switch"
-                                                                     andProfile:^(BlueCapCharacteristicProfile* characteristicProfile) {
-                                                                     }];
-                                   [serviceProfile createCharacteristicWithUUID:@"2f0a0019-69aa-f316-3e78-4194989a6c1a"
-                                                                           name:@"Sunset Switch"
-                                                                     andProfile:^(BlueCapCharacteristicProfile* characteristicProfile) {
-                                                                     }];
-                                   [serviceProfile createCharacteristicWithUUID:@"2f0a0020-69aa-f316-3e78-4194989a6c1a"
-                                                                           name:@"Sunrise Switch"
-                                                                     andProfile:^(BlueCapCharacteristicProfile* characteristicProfile) {
-                                                                     }];
-                               }];
-    
 #pragma mark - Scenes Service -
 
-    [profileManager createServiceWithUUID:HUE_LIGHTS_SCENES_SERVICE_UUID
+    [profileManager createServiceWithUUID:HUE_LIGHTS_SERVICE_UUID
                                      name:@"Hue Lights Scenes"
                                andProfile:^(BlueCapServiceProfile* serviceProfile) {
                                    [serviceProfile createCharacteristicWithUUID:@"2f0a0012-69aa-f316-3e78-4194989a6c1a"
                                                                            name:@"Number of Scenes"
                                                                      andProfile:^(BlueCapCharacteristicProfile* characteristicProfile) {
+                                                                         characteristicProfile.properties = CBCharacteristicPropertyRead;
+                                                                         [characteristicProfile deserializeData:^NSDictionary*(NSData* data) {
+                                                                             return @{HUE_LIGHTS_NUMBER_OF_SCENES:blueCapUnsignedCharFromData(data, NSMakeRange(0, 1))};
+                                                                         }];
+                                                                         [characteristicProfile stringValue:^NSDictionary*(NSDictionary* data) {
+                                                                             return @{HUE_LIGHTS_NUMBER_OF_SCENES:[[data objectForKey:HUE_LIGHTS_NUMBER_OF_SCENES] stringValue]};
+                                                                         }];
+                                                                         [characteristicProfile serializeString:^NSData*(NSDictionary* data) {
+                                                                             uint8_t value = [[data objectForKey:HUE_LIGHTS_NUMBER_OF_SCENES] intValue];
+                                                                             return blueCapUnsignedCharToData(value);
+                                                                         }];
+                                                                         characteristicProfile.initialValue = [characteristicProfile valueFromString:@{HUE_LIGHTS_NUMBER_OF_SCENES:[NSString stringWithFormat:@"%d", 10]}];
                                                                      }];
                                    [serviceProfile createCharacteristicWithUUID:@"2f0a0013-69aa-f316-3e78-4194989a6c1a"
                                                                            name:@"Scene ID"
                                                                      andProfile:^(BlueCapCharacteristicProfile* characteristicProfile) {
+                                                                         characteristicProfile.properties = CBCharacteristicPropertyRead | CBCharacteristicPropertyWrite;
+                                                                         [characteristicProfile serializeObject:^NSData*(id data) {
+                                                                             uint8_t value = [data intValue];
+                                                                             return blueCapUnsignedCharToData(value);
+                                                                         }];
+                                                                         [characteristicProfile deserializeData:^NSDictionary*(NSData* data) {
+                                                                             return @{HUE_LIGHTS_SCENE_ID:blueCapUnsignedCharFromData(data, NSMakeRange(0, 1))};
+                                                                         }];
+                                                                         [characteristicProfile stringValue:^NSDictionary*(NSDictionary* data) {
+                                                                             return @{HUE_LIGHTS_SCENE_ID:[[data objectForKey:HUE_LIGHTS_SCENE_ID] stringValue]};
+                                                                         }];
+                                                                         [characteristicProfile serializeString:^NSData*(NSDictionary* data) {
+                                                                             uint8_t value = [[data objectForKey:HUE_LIGHTS_SCENE_ID] intValue];
+                                                                             return blueCapUnsignedCharToData(value);
+                                                                         }];
+                                                                         characteristicProfile.initialValue = [characteristicProfile valueFromString:@{HUE_LIGHTS_SCENE_ID:[NSString stringWithFormat:@"%d", 1]}];
                                                                      }];
                                    [serviceProfile createCharacteristicWithUUID:@"2f0a0014-69aa-f316-3e78-4194989a6c1a"
                                                                            name:@"Scene Name"
                                                                      andProfile:^(BlueCapCharacteristicProfile* characteristicProfile) {
+                                                                         characteristicProfile.properties = CBCharacteristicPropertyRead | CBCharacteristicPropertyWrite;
+                                                                         [characteristicProfile serializeObject:^NSData*(id data) {
+                                                                             return nil;
+                                                                         }];
+                                                                         [characteristicProfile afterDiscovered:^(BlueCapCharacteristic* chararacteristic) {
+                                                                         }];
+                                                                         [characteristicProfile deserializeData:^NSDictionary*(NSData* data) {
+                                                                             return @{};
+                                                                         }];
+                                                                         [characteristicProfile stringValue:^NSDictionary*(NSDictionary* data) {
+                                                                             return @{};
+                                                                         }];
+                                                                         [characteristicProfile serializeString:^NSData*(NSDictionary* data) {
+                                                                             return nil;
+                                                                         }];
+                                                                         characteristicProfile.initialValue = [characteristicProfile valueFromString:@{}];
                                                                      }];
                                    [serviceProfile createCharacteristicWithUUID:@"2f0a0015-69aa-f316-3e78-4194989a6c1a"
                                                                            name:@"Current Scene ID"
                                                                      andProfile:^(BlueCapCharacteristicProfile* characteristicProfile) {
+                                                                         characteristicProfile.properties = CBCharacteristicPropertyRead | CBCharacteristicPropertyWrite;
+                                                                         [characteristicProfile serializeObject:^NSData*(id data) {
+                                                                             return nil;
+                                                                         }];
+                                                                         [characteristicProfile deserializeData:^NSDictionary*(NSData* data) {
+                                                                             return @{};
+                                                                         }];
+                                                                         [characteristicProfile stringValue:^NSDictionary*(NSDictionary* data) {
+                                                                             return @{};
+                                                                         }];
+                                                                         [characteristicProfile serializeString:^NSData*(NSDictionary* data) {
+                                                                             return nil;
+                                                                         }];
+                                                                         characteristicProfile.initialValue = [characteristicProfile valueFromString:@{}];
                                                                      }];
                                    [serviceProfile createCharacteristicWithUUID:@"2f0a0025-69aa-f316-3e78-4194989a6c1a"
                                                                            name:@"Number of Lights"
                                                                      andProfile:^(BlueCapCharacteristicProfile* characteristicProfile) {
+                                                                         characteristicProfile.properties = CBCharacteristicPropertyRead;
+                                                                         [characteristicProfile deserializeData:^NSDictionary*(NSData* data) {
+                                                                             return @{};
+                                                                         }];
+                                                                         [characteristicProfile stringValue:^NSDictionary*(NSDictionary* data) {
+                                                                             return @{};
+                                                                         }];
+                                                                         [characteristicProfile serializeString:^NSData*(NSDictionary* data) {
+                                                                             return nil;
+                                                                         }];
+                                                                         characteristicProfile.initialValue = [characteristicProfile valueFromString:@{}];
                                                                      }];
                                    [serviceProfile createCharacteristicWithUUID:@"2f0a0022-69aa-f316-3e78-4194989a6c1a"
                                                                            name:@"Light Color"
                                                                      andProfile:^(BlueCapCharacteristicProfile* characteristicProfile) {
-                                                                     }];
-                                   [serviceProfile createCharacteristicWithUUID:@"2f0a0025-69aa-f316-3e78-4194989a6c1a"
-                                                                           name:@"Next Scene"
-                                                                     andProfile:^(BlueCapCharacteristicProfile* characteristicProfile) {
+                                                                         characteristicProfile.properties = CBCharacteristicPropertyRead | CBCharacteristicPropertyWrite;
+                                                                         [characteristicProfile serializeObject:^NSData*(id data) {
+                                                                             return nil;
+                                                                         }];
+                                                                         [characteristicProfile deserializeData:^NSDictionary*(NSData* data) {
+                                                                             return @{};
+                                                                         }];
+                                                                         [characteristicProfile stringValue:^NSDictionary*(NSDictionary* data) {
+                                                                             return @{};
+                                                                         }];
+                                                                         [characteristicProfile serializeString:^NSData*(NSDictionary* data) {
+                                                                             return nil;
+                                                                         }];
+                                                                         characteristicProfile.initialValue = [characteristicProfile valueFromString:@{}];
                                                                      }];
                                    [serviceProfile createCharacteristicWithUUID:@"2f0a0024-69aa-f316-3e78-4194989a6c1a"
                                                                            name:@"Operation"
                                                                      andProfile:^(BlueCapCharacteristicProfile* characteristicProfile) {
+                                                                         characteristicProfile.properties = CBCharacteristicPropertyRead | CBCharacteristicPropertyWrite;
+                                                                         [characteristicProfile serializeObject:^NSData*(id data) {
+                                                                             return nil;
+                                                                         }];
+                                                                         [characteristicProfile deserializeData:^NSDictionary*(NSData* data) {
+                                                                             return @{};
+                                                                         }];
+                                                                         [characteristicProfile stringValue:^NSDictionary*(NSDictionary* data) {
+                                                                             return @{};
+                                                                         }];
+                                                                         [characteristicProfile serializeString:^NSData*(NSDictionary* data) {
+                                                                             return nil;
+                                                                         }];
+                                                                         characteristicProfile.initialValue = [characteristicProfile valueFromString:@{}];
                                                                      }];
-                                   [serviceProfile createCharacteristicWithUUID:@"2f0a0026-69aa-f316-3e78-4194989a6c1a"
-                                                                           name:@"Next Light"
+                                   [serviceProfile createCharacteristicWithUUID:@"2f0a0027-69aa-f316-3e78-4194989a6c1a"
+                                                                           name:@"Switches"
                                                                      andProfile:^(BlueCapCharacteristicProfile* characteristicProfile) {
+                                                                         characteristicProfile.properties = CBCharacteristicPropertyRead | CBCharacteristicPropertyWrite;
+                                                                         [characteristicProfile serializeObject:^NSData*(id data) {
+                                                                             return nil;
+                                                                         }];
+                                                                         [characteristicProfile deserializeData:^NSDictionary*(NSData* data) {
+                                                                             return @{};
+                                                                         }];
+                                                                         [characteristicProfile stringValue:^NSDictionary*(NSDictionary* data) {
+                                                                             return @{};
+                                                                         }];
+                                                                         [characteristicProfile serializeString:^NSData*(NSDictionary* data) {
+                                                                             return nil;
+                                                                         }];
+                                                                         characteristicProfile.initialValue = [characteristicProfile valueFromString:@{}];
                                                                      }];
                                }];
 
